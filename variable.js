eb3ae5791cb15420935ddec276204048dca56fc0
sharmin <sharminmoon2@gmail.com>
2019-07-03 00:05:35 +0600

    add variablejs and some varaible exercies

---
+++ variable.js
@@ -0,0 +1,10 @@
+var variableName;
+let variableName1;
+//const variableNameTwo;
+
+variableName = "Sharmin moon";
+console.log(variableName);
+
+variableName1 = "Another vairable";
+
+console.log(variableName1);
